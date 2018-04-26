991e1234081d742a40a3fe79762c5c9e6785e1a2
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2018-04-26 20:57:49 +0530

    nytimes, must still complete later

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,7 +1,14 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
+    function addImage(){
+    	debugger;
+    }
     let searchedForText;
+    const unsplashRequest=new XMLHttpRequest();
+    unsplashRequest.open('GET',`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=25a10cd1a8ad4e70a042baf298711fcf`);
+    unsplashRequest.onload = addImage;
+    unsplashRequest.send();
     const responseContainer = document.querySelector('#response-container');
 
     form.addEventListener('submit', function (e) {
