fc5f92718fb2ab9288ea54b5a84e9eaf98dc810a
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2018-04-28 17:12:23 +0530

    ajax code with jquery, still dosent work

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -2,14 +2,21 @@
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
     function addImage(){
-    	debugger;
+    	$("#response-container").append("responses")
     }
     let searchedForText;
-    const unsplashRequest=new XMLHttpRequest();
+    var settings={
+        url:`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}`,
+        headers:{
+            Authorization:'Client-ID 25a10cd1a8ad4e70a042baf298711fcf'
+        }
+    }
+    $.ajax(settings).done(addImage);
+    /*const unsplashRequest=new XMLHttpRequest();
     unsplashRequest.open('GET',`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=25a10cd1a8ad4e70a042baf298711fcf`);
     unsplashRequest.onload = addImage;
     unsplashRequest.send();
-    const responseContainer = document.querySelector('#response-container');
+    const responseContainer = document.querySelector('#response-container');*/
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
